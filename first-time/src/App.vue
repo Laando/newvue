633a8d12a09cd9fb5c6936fee35f39efdc437925
lan <tienlando2704@gmail.com>
2021-10-20 09:29:19 +0700

    first commit

---
+++ first-time/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="message">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'TEST',
+  data() {
+    return {
+      message: 'Hãy sửa thông điệp này'
+    }
+  },
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
